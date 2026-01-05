a3c9731e24cc3f7c3e7cd62dd744f234905fe56b
Adnan <adnan.c2208@gmail.com>
2026-01-05 21:49:18 +0530

    Hardcoded length of array fixed

--- Backend/src/RandomSongPicker.ts
+++ Backend/src/RandomSongPicker.ts
@@ -1,11 +1,12 @@
 import Allsongs from "../dataExtraction/AllSongs.json" with {type : "json"}
 
+const AllSongsLenght = Allsongs.length;
 type Song = {
     Title: string,
     Lyric: string
 }
 
-let RandomSongIndex: number = Math.floor(Math.random() * 318);
+let RandomSongIndex: number = Math.floor(Math.random() * AllSongsLenght);
 const songsArray: Song[] = Allsongs as Song[]
 
 export default songsArray[RandomSongIndex]
