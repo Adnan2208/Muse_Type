a691f86f013e4c57b990294a1483938bfbc26273
Adnan <adnan.c2208@gmail.com>
2026-01-16 22:31:54 +0530

    Typing and cursor basic functionality added

--- Frontend/src/components/RandomSong.tsx
+++ Frontend/src/components/RandomSong.tsx
@@ -1,9 +1,12 @@
 import getSong,{type Song} from "../axios";
-import { useEffect,useState } from "react";
+import { useEffect,useState,useRef } from "react";
 
 function RandomSong(){
 
 const [songState,setSongState] = useState<null | Song>()
+const [i, setI] = useState<number>(0)
+const divRef = useRef<HTMLDivElement>(null)
+
 useEffect(() => {
     const fetchSong = async () => {
       const song : Song = await getSong();
@@ -12,18 +15,40 @@ useEffect(() => {
     fetchSong();
   },[])
 
+useEffect(() => {
+  divRef.current?.focus();
+}, [])
+
 const totalSong = songState?.Lyric.slice(0,600)
 const actualSong = totalSong?.slice(0,totalSong.lastIndexOf(" "))
 
+
+function checkIfCorrect(e : React.KeyboardEvent){
+  const key = e.key;
+  if (key === actualSong?.[i]) {
+    console.log(key);
+  } else {
+    console.log("wrong");
+  }
+  setI(i => i+1);
+}
+
 return(
   <>
-    <div className="flex items-center">
-      <div className="px-16 py-8 flex items-center text-gray-700 font-mono text-2xl max-w-5xl w-full text-center leading-relaxed rounded-2xl shadow-lg mx-auto bg-orange-50">
+    <section className="flex items-center">
+      <div className="py-8 px-16 flex text-black font-mono text-2xl max-w-5xl w-full leading-relaxed rounded-2xl shadow-lg mx-auto bg-orange-50 items-start gap-0 outline-none border-0 flex-wrap whitespace-pre" onKeyDown={(e) => checkIfCorrect(e)} tabIndex={0} ref={divRef}>
+        <span className="cursor-blink">┃</span>
+
+        {/* Fix by converting each word to a span rather than single characters */}
         {
-          actualSong
+          actualSong?.split("").map((char,i) => { 
+            return(
+            <span key={i} className="">{char}</span>)
+        })
         }
+
       </div>
-    </div>
+    </section>
   </>
 )
 }
